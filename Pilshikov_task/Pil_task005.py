c3905a5fa9082827544171fc3e6d8c04324fc238
Mikhail Popkov <mihel-98@yandex.ru>
2021-03-11 00:48:37 +0300

    refactor: odd space

--- Pilshikov_task/Pil_task005.py
+++ Pilshikov_task/Pil_task005.py
@@ -7,7 +7,15 @@ Task 2.10 page 12
 
 """
 
-input_numb = float(input("Enter number type - float,to check the condition\n х ∈ [0,1] ?: "))
-# checking the condition
-print('Yes' if 0 < input_numb < 1 else 'No')
 
+def checking_condition(number: float) -> str:
+    """
+    This function checking whether the number is contained in the specified interval
+    :param number: type float
+    :return: answer "Yes" or "No"
+    """
+    return 'Yes' if 0 < number < 1 else 'No'
+
+
+enter_number = float(input("Enter number type - float,to check the condition\n х ∈ [0,1] ?: "))
+print(checking_condition(enter_number))
