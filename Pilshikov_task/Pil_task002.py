2e8cfd0dacacdaf929f17668d00447a05041c557
Mikhail Popkov <mihel-98@yandex.ru>
2021-03-10 23:54:09 +0300

    refactor: rename input variable in function

---
+++ Pilshikov_task/Pil_task002.py
@@ -0,0 +1,22 @@
+#  Copyright (c) 2021.
+#  Mikhail Popkov (mihel-98@yandex.ru)
+"""
+
+Task 1.14  page 8
+write formula
+
+"""
+import math
+
+
+def formula_task_pilshikov_1_14(x: int) -> float:
+    """
+        input: number type integer
+        This function calculate by formula from task Pilshikov
+        output: number type float
+    """
+    return math.pow((1 + x), 2)
+
+
+input_numb = int(input('Calculated formula  - (1+x)^2\nEnter integer x: '))
+print('Answer:', formula_task_pilshikov_1_14(input_numb))
