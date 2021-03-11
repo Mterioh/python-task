dc38c12060ea2f8e6f55dc72a90e00d7e412c40a
Mikhail Popkov <mihel-98@yandex.ru>
2021-03-11 03:28:47 +0300

    fix: add function answer and refactor code

--- Pilshikov_task/Pil_task008.py
+++ Pilshikov_task/Pil_task008.py
@@ -8,13 +8,21 @@ are any of the digits of a three-digit number the same
 """
 
 
-def check_number_digit(number):
-    """check number of digit"""
+def check_number_digit(number: int) -> bool:
+    """
+    check number of digit
+    :param number: type integer
+    :return: True if condition is met, else False
+    """
     return len(str(number)) == 3
 
 
-def divides_number(number):
-    """divides the number by the digits"""
+def divides_number(number: int) -> list:
+    """
+    divides the number by the digits
+    :param number: type integer
+    :return: list digits
+    """
     digit_series = []
     while number > 0:
         digit_series.append(number % 10)
@@ -22,22 +30,28 @@ def divides_number(number):
     return digit_series
 
 
-input_number = int(input("This program are any of the digits of a three-digit number the same\nEnter numb: "))
+def search_duplicate_number(digits: list):
+    """
+    :param digits: type list
+    """
+    if digits[0] == digits[1] or digits[1] == digits[2] \
+            or digits[2] == digits[0]:
+        print('Yes')
+    else:
+        print('No')
+
+
+enter_number = int(input("This program are any of the digits of a three-digit number the same\nEnter numb: "))
 
 while True:
     """Recursive function call to check a number"""
-    check_number = check_number_digit(input_number)
-    if check_number:
+    check_entered = check_number_digit(enter_number)
+    if check_entered:
         break
     else:
-        check_number = check_number_digit(input_number)
-        input_number = int(input("Please, enter a three-digit number: "))
+        check_entered = check_number_digit(enter_number)
+        enter_number = int(input("Please, enter a three-digit number: "))
 
 # Call function divides_number
-revers_digit_series = divides_number(input_number)
-# turning list
-revers_digit_series[::-1]
-# search duplicate numbers
-print('Yes' if revers_digit_series[0] == revers_digit_series[1] \
-               or revers_digit_series[1] == revers_digit_series[2] \
-               or revers_digit_series[2] == revers_digit_series[0] else 'No')
+list_digit = divides_number(enter_number)
+search_duplicate_number(list_digit[::-1])
