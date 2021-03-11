263342d64163e466c472f89afe9545677269220c
Mikhail Popkov <mihel-98@yandex.ru>
2021-03-11 04:02:40 +0300

    fix:change description functions and add new function in task 9, 10

--- Pilshikov_task/Pil_task009.py
+++ Pilshikov_task/Pil_task009.py
@@ -7,6 +7,20 @@ condition for the existence of a triangle
 
 """
 
-# x, y, z - side of a triangle
-x, y, z = int(input("input side x: ")), int(input("input side y: ")), int(input("input side z: "))
-print('Yes' if (x < y + z) and (y < x + z) and (z < x + y) else 'No')
+
+def existence_triangle(x: int, y: int, z: int):
+    """
+    This function calculate value due to description in Pilschikov book, task 3.19, page 18
+    :param x: side triangle x type integer
+    :param y: side triangle y type integer
+    :param z: side triangle z type integer
+    """
+    if (x < y + z) and (y < x + z) and (z < x + y):
+        print('Yes')
+    else:
+        print('No')
+
+
+enter_x = int(input("This program condition for the existence of a triangle\nenter side x: "))
+enter_y = int(input("enter side y: "))
+enter_z = int(input("enter side z: "))
